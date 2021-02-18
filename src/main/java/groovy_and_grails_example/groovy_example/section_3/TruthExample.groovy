7664fe7010072599cd9325ac604410311509081e
Мазурук Олег Володимирович <Oleg.Mazuruk@innovinnprom.com>
2021-02-18 16:20:12 +0200

    pass: truth

---
+++ src/main/java/groovy_and_grails_example/groovy_example/section_3/TruthExample.groovy
@@ -0,0 +1,14 @@
+package groovy_and_grails_example.groovy_example.section_3
+
+// Truth in groovy
+assert true
+assert !false
+
+assert ![]
+assert [1]
+
+assert !0
+assert 48
+
+assert !''
+assert 'str'
